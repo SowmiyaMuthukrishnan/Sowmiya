8058e92c394700b0abdae3177f35a6ce30a3b340
SowmiyaMuthukrishnan <141564940+SowmiyaMuthukrishnan@users.noreply.github.com>
2023-08-09 14:16:55 +0530

    Add files via upload

---
+++ Basics/basics.py
@@ -0,0 +1,38 @@
+#to print the given string
+x="Hello world"
+print(x)
+
+#adding two numbers
+x=2
+y=3
+z=x+y
+print("Z=",z)
+
+#swap two numbers
+x=5
+y=7
+x,y=y,x
+print("X=",x)
+print("Y=",y)
+
+#types
+#example 1
+x=2
+print(type(x))
+
+#example 2
+x=["Sowmiya"]
+print(type(x))
+
+#example 3
+x=("Sowmiya")
+print(type(x))
+
+#example 4
+x={"Name":"Sowmiya"}
+print(type(x))
+
+
+
+
+
